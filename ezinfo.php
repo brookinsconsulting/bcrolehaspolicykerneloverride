c9e384b4b37b96e7c7ef19eee09815722aee24a6
Brookins Consulting <info@brookinsconsulting.com>
2012-01-22 15:28:01 -0600

    Added: Initial import of bcrolehaspolicykerneloverride extension (based on share.ez.no forum gist collaboration) at version 0.0.2

---
+++ ezinfo.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * File containing the bcrolehaspolicykerneloverrideInfo class.
+ *
+ * @copyright Copyright (C) 1999 - 2012 Brookins Consulting. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
+ * @version //autogentag//
+ * @package bcrolehaspolicykerneloverride
+ */
+class bcrolehaspolicykerneloverrideInfo
+{
+    function info()
+    {
+        return array(
+               'Name' => "<a href='http://projects.ez.no/bcrolehaspolicykerneloverride'>BC Role hasPolicy Kernel Override</a>",
+               'Version' => "0.0.1",
+               'Copyright' => "Copyright (C) 1999 - 2012 <a href='http://ez.no'>eZ Systems AS</a> and <a href='http://brookinsconsulting.com'>Brookins Consulting</a>",
+               'Author' => "Brookins Consulting",
+               'License' => "GNU General Public License"
+        );
+    }
+}
+?>
